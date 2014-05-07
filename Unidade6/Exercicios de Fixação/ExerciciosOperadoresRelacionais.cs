43f0d2af84d03e7cb37e4d1be8c6351f95e093e1
Felipovisk <felipe.dellacosta@gmail.com>
2014-05-07 09:28:36 -0300

    adicionado exercicios slide unidade 7

---
+++ Unidade6/Exercicios de Fixação/ExerciciosOperadoresRelacionais.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unidade6.Exercicios_de_Fixação
+{
+    class ExerciciosOperadoresRelacionais
+    {
+        public static int x = 50;
+        public static int y = 40;
+        public static void Main(String[] args)
+        {
+            x = y;
+            x += y;
+            x -= y;
+            x *= y;
+            x /= y;
+            x %= y;
+            x &= y;
+            x |= y;
+            
+            
+            
+
+        }
+    }
+}
