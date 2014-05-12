0deb30a42ab98c608b531d629d409d58a099b6ec
Felipovisk <felipe.dellacosta@gmail.com>
2014-05-12 09:17:41 -0300

    corrigido questao 8 e 9

---
+++ MedindoaFebreV/Questao8.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MedindoaFebreV
+{
+    internal class Questao8
+    {
+        public static int Cont = 0;
+
+        public static void Main1(String[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Digite SOMENTE a HORA de entrada do cliente.");
+                int horario = int.Parse(Console.ReadLine());
+                if (horario >= 10 & horario <= 16)
+                {
+                    Cont++;
+                }
+                else
+                {
+                    Console.WriteLine("horario Inválido.");
+                }
+            }
+            Console.WriteLine("Clientes que foram atendidos dentro do horário de funcionamento: " + Cont);
+            Console.ReadKey();
+        }
+    }
+}
