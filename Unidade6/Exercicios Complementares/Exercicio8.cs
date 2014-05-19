949e42d6589a2421f3ee54f0bb4084dadc26df50
Felipovisk <felipe.dellacosta@gmail.com>
2014-05-19 13:27:54 -0300

    finalizado exercicio2

--- Unidade6/Exercicios Complementares/Exercicio8.cs
+++ Unidade6/Exercicios Complementares/Exercicio8.cs
@@ -6,10 +6,10 @@ using System.Threading.Tasks;
 
 namespace Unidade6.Exercicios_Complementares
 {
-    /*Quando se lista o nome de autores de livros, artigos e outras publicações 
-     é comum que se apresente o nome do autor ou dos autores da seguinte forma: 
-     sobrenome do autor em letras maiúsculas, seguido de uma vírgula e da primeira 
-     parte do nome apenas com as iniciais maiúsculas.
+/*Quando se lista o nome de autores de livros, artigos e outras publicações 
+é comum que se apresente o nome do autor ou dos autores da seguinte forma: 
+sobrenome do autor em letras maiúsculas, seguido de uma vírgula e da primeira 
+parte do nome apenas com as iniciais maiúsculas.
 Por exemplo:
 SILVA, Joao
 COELHO, Paulo
--- Unidade7/ExerciciosFixação1/ExercicioFixacao2.cs
+++ Unidade7/ExerciciosFixação1/ExercicioFixacao2.cs
@@ -21,29 +21,43 @@ Obs.: O final da leituras dos dados se dará com a entrada de um “salário neg
         public static int FilhosCidadao = 0;
         public static double MediaSalario = 0;
         public static double MediaFilhos = 0;
+        public static int ContSalarioMenor150 = 0;
+        public static double PercentualSalario = 0;
+        public static double Aux = 0;
         public static void Main(String[] args)
         {
             Console.WriteLine("Digite o salario do cidadão.");
             SalarioCidadao = double.Parse(Console.ReadLine());
             while (SalarioCidadao >= 0)
             {
-                int cont = 0;
+                
+                double cont = 0;
                 cont ++;
                 Console.WriteLine("Digite quantos filhos o cidadão possui.");
                 FilhosCidadao = int.Parse(Console.ReadLine());
                 MediaSalario += SalarioCidadao / cont;
-                int contfilhos = 0; contfilhos += FilhosCidadao;
+                double contfilhos = 0; contfilhos += FilhosCidadao;
                 MediaFilhos = contfilhos / cont;
-                double aux = 0;
-                if (SalarioCidadao > aux)
+                
+                if (SalarioCidadao > Aux)
                 {
-                    aux = SalarioCidadao;
+                    Aux = SalarioCidadao;
+                }
+                else if (SalarioCidadao < 150)
+                {
+                    ContSalarioMenor150++;
+                    PercentualSalario = (cont * ContSalarioMenor150) / 100;
                 }
 
 
                 Console.WriteLine("Digite o salario do cidadão.");
                 SalarioCidadao = double.Parse(Console.ReadLine());
             }
+            Console.WriteLine("A media de salario da população é de: " + MediaSalario + " Reais.");
+            Console.WriteLine("A media de filhos da população é de: " + MediaFilhos + " filhos por cidadão.");
+            Console.WriteLine("O maior salario dos habitantes é de: " + Aux + " Reais.");
+            Console.WriteLine("Percentual de cidadões com salário abaixo de R$ 150,00 é de: " + PercentualSalario + " %.");
+            Console.ReadKey();
             
         }
     }
