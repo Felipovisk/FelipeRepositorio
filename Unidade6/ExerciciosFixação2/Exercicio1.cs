39c71a3ce0c1c33372ac95ce38d4b41210a58d03
Felipovisk <felipe.dellacosta@gmail.com>
2014-05-26 09:50:31 -0300

    finalizado exercicio 1

--- Unidade6/ExerciciosFixação2/Exercicio1.cs
+++ Unidade6/ExerciciosFixação2/Exercicio1.cs
@@ -1,19 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Unidade6.ExerciciosFixação2
 {
-/*Declarar um vetor de 20 números inteiros, em seguida ler os elementos do vetor, 
-* e ao final mostrar somente os número positivos, e mostrar ainda a quantidade de 
-* positivos, a quantidade de negativos e a quantidade de nulos (0). */
-    class Exercicio1
+    /*Declarar um vetor de 20 números inteiros, em seguida ler os elementos do vetor,
+    * e ao final mostrar somente os número positivos, e mostrar ainda a quantidade de
+    * positivos, a quantidade de negativos e a quantidade de nulos (0). */
+
+    internal class Exercicio1
     {
         public static int[] NumerosInteiros = new int[20];
-        public static int[] NumerosPositivos;
+        public static int[] NumerosPositivos = new int[20];
         public static int[] Contador = new int[3];
+
+        //FAKE
+        //public static int[] NumerosInteiros = new int[]{0,0,0,0,0,0,0,4,4,4,4,4,4,4,-9,-9,-9,-9,-9,-9};
+        //public static int[] NumerosPositivos = new int[20];
+        //public static int[] Contador = new int[3];
 
         public static void Main(String[] args)
         {
@@ -24,6 +26,7 @@ namespace Unidade6.ExerciciosFixação2
                 if (NumerosInteiros[i] > 0)
                 {
                     Contador[0] += 1;
+                    NumerosPositivos[i] = NumerosInteiros[i];
                 }
                 else if (NumerosInteiros[i] < 0)
                 {
@@ -33,22 +36,21 @@ namespace Unidade6.ExerciciosFixação2
                 {
                     Contador[2] += 1;
                 }
+            }            
+
+            for (int j = 0; j < NumerosPositivos.Length; j++)
+            {
+                if (NumerosPositivos[j] > 0)
+                {
+                    Console.WriteLine("Numeros Positivos: " + NumerosPositivos[j]);
+                }
                 
             }
-            NumerosPositivos = new int[Contador[0]];
-            for (int i = 0; i < NumerosPositivos.Length; i++)
-            {
-                if (NumerosInteiros[i] > 0)
-                {
-                    NumerosPositivos[i] = NumerosInteiros[i];
-                }
-                Console.WriteLine("Numeros Positivos: " + NumerosPositivos[i]);
-            }
+
             Console.WriteLine("Total de numeros positivos: " + Contador[0]);
             Console.WriteLine("Total de numeros negativos: " + Contador[1]);
             Console.WriteLine("Total de numeros nulos: " + Contador[2]);
             Console.ReadKey();
-            
         }
     }
 }
