63b9fbe4c2a9320222c802df0e9ebb61371fc0f3
Felipovisk <felipe.dellacosta@gmail.com>
2014-06-26 20:04:50 -0300

    adicionado exercicio 5.

---
+++ UnidadeComplementar/Exercicio5.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnidadeComplementar
+{/*5)	Crie um algoritmo com uma coleção com todos dos times da copa. Sabe-se que são 64 
+  * times que serão sorteados e divididos em 8 grupos, do A ao H. Depois de sorteados e 
+  * alocados o algoritmo deve imprimir na tela todos os grupos.*/
+    class Exercicio5
+    {
+        static public Dictionary<int, string> GruposCopa = new Dictionary<int, string>();
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
