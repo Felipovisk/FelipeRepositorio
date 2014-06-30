0dab582ee0efb5c5d294db0eb082340e196e6b60
Felipovisk <felipe.dellacosta@gmail.com>
2014-06-30 08:53:11 -0300

    finalizado exercicio 4 unidade 10

--- Unidade10/Exercicio4.cs
+++ Unidade10/Exercicio4.cs
@@ -11,7 +11,19 @@ namespace Unidade10
     {
         public static void Main(string[] args)
         {
+            int tabuada = 4;
 
+            TabuadaPorParametro(tabuada);
+            Console.ReadKey();
+        }
+
+        private static void TabuadaPorParametro(int tabuada)
+        {
+            for (int i = 1; i < 11; i++)
+            {
+                int result = tabuada * i;
+                Console.WriteLine(result);
+            }
         }
     }
 }
